7e3a524bc9e1bf84a73c738101fbd87afb49b51d
petersamokhin <contact@petersamokhin.com>
2019-09-14 10:57:41 +0300

    some comments

--- main.go
+++ main.go
@@ -8,9 +8,24 @@ import (
 )
 
 const (
+	// Target directory in which will all godoc
+	// pages will be saved.
+	// (only few html files related to your project,
+	// and 2-3 .css & .js files of a godoc site)
+	// It's a directory inside of a docker container.
 	docsDir                = "/app/docs"
+
+	// Need to replace some absolute links 
+	// in all output files, see the replacer.json config
 	replacerFilesPattern   = "*.html"
+
+    // Path to a replacer config.
+    // See README.md and replacer.go
+    // It's a file inside of a docker container.
 	replacerConfigFilePath = "/app/replacer.json"
+
+	// Don't matter the port, 
+	// but it's necessary to know it for downloading.
 	godocPort              = 6060
 
 	// it is necessary to wait some time
